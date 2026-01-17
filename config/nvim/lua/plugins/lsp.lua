652167ac81e50460cf2252e0fee6768c5a2e95e5
fiore57 <fiore57.cpp@gmail.com>
2026-01-18 02:36:09 +0900

    feat: HaskellのLSP環境（HLS）を構築
    - HLSの互換性のため、GHCを9.12.2に固定

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,28 @@
 return {
   {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "lua_ls" },
-    },
+    "neovim/nvim-lspconfig",
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
+      "mason-org/mason-lspconfig.nvim",
     },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
+      })
+      vim.lsp.enable("lua_ls")
+      vim.lsp.config["hls"] = {
+        install = {
+          cmd = { "haskell-language-server-wrapper", "--lsp" },
+        },
+        -- どのファイルがあったらプロジェクトとみなすか
+        root_markers =  { "cabal.project", "stack.yaml", "package.yaml", ".git" },
+        settings = {
+          haskell = {
+            formattingProvider = "ormolu",
+          }
+        }
+      }
+      vim.lsp.enable("hls")
+    end,
   }
 }
