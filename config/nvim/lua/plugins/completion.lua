0862206bff81ca66c1bbe6e8fdf0a00cc57b31d5
fiore57 <fiore57.cpp@gmail.com>
2026-04-28 11:06:22 +0900

    chore: nvimの補完の操作方法をsuper-tabに変更

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -7,13 +7,12 @@ return {
 			-- キーマップ
 			-- <C-Space>：補完開始
 			-- <C-e>：隠す
-			-- <C-y>：決定
-			-- <Tab>：スニペットジャンプ（次へ）
+			-- <Tab>：決定/スニペットジャンプ（次へ）
 			-- <S-Tab>：スニペットジャンプ（戻る）
 			-- <Up><Down>or<C-p><C-n>：候補選択
 			-- <C-b><C-f>：ドキュメントをスクロール
 			-- <C-k>：signature help window（関数呼び出し時に表示される情報）の表示/非表示を切り替え
-			keymap = { preset = "default" },
+			keymap = { preset = "super-tab" },
 			-- 補完ソース
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer" },
