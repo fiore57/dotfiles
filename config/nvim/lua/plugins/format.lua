ccd67cc7e632b3d30d87c0c28d7208c506444fbe
fiore57 <fiore57.cpp@gmail.com>
2026-06-15 00:47:08 +0900

    chore: .vue等をprettierでformatするよう変更

--- config/nvim/lua/plugins/format.lua
+++ config/nvim/lua/plugins/format.lua
@@ -10,10 +10,16 @@ return {
 			javascriptreact = { "prettier" },
 			typescript = { "prettier" },
 			typescriptreact = { "prettier" },
+			vue = { "prettier" },
 
 			json = { "prettier" },
+			jsonc = { "prettier" },
 			yaml = { "prettier" },
 			markdown = { "prettier" },
+
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
